00bb20dc44a235af8ed962915c2bce585fb766e9
Eric Holk <eholk@cs.indiana.edu>
2014-07-15 16:59:33 -0400

    Adding a vectorized version, and experimenting with different SM settings.

--- cuda-nbody/nbody-vector.cu
+++ cuda-nbody/nbody-vector.cu
@@ -7,51 +7,44 @@
 #include <iostream>
 using namespace std;
 
-struct point3 {
-	int32_t tag; // Matches the tag field in Harlan
-	float x;
-	float y;
-	float z;
-};
-
-float __device__ mag(point3 p) {
+float __device__ mag(float3 p) {
 	return sqrt(p.x * p.x + p.y * p.y + p.z * p.z);
 }
 
-point3 __device__ operator-(point3 lhs, point3 rhs) {
-	point3 result;
+float3 __device__ operator-(float3 lhs, float3 rhs) {
+	float3 result;
 	result.x = lhs.x - rhs.x;
 	result.y = lhs.y - rhs.y;
 	result.z = lhs.z - rhs.z;
 	return result;
 }
 
-point3 __device__ operator+(point3 lhs, point3 rhs) {
-	point3 result;
+float3 __device__ operator+(float3 lhs, float3 rhs) {
+	float3 result;
 	result.x = lhs.x + rhs.x;
 	result.y = lhs.y + rhs.y;
 	result.z = lhs.z + rhs.z;
 	return result;
 }
 
-point3 __device__ operator/(point3 lhs, float rhs) {
-	point3 result;
+float3 __device__ operator/(float3 lhs, float rhs) {
+	float3 result;
 	result.x = lhs.x / rhs;
 	result.y = lhs.y / rhs;
 	result.z = lhs.z / rhs;
 	return result;
 }
 
-__global__ void nbody(int N, point3 *bodies, point3 *result) {
+__global__ void nbody(int N, float3 *bodies, float3 *result) {
 	int i = blockDim.x * blockIdx.x + threadIdx.x;
 	
 	if(i >= N) return;
 
-	point3 total = {0};
-	point3 base = bodies[i];
+	float3 total = {0};
+	float3 base = bodies[i];
 
 	for(int j = 0; j < N; ++j) {
-		point3 diff = bodies[j] - base;
+		float3 diff = bodies[j] - base;
 		float d = mag(diff);
 		
 		if(d > 0) {
@@ -62,16 +55,15 @@ __global__ void nbody(int N, point3 *bodies, point3 *result) {
 }
 
 
-point3 *mkbodies(int N) {
-	point3 *bodies = new point3[N];
+float3 *mkbodies(int N) {
+	float3 *bodies = new float3[N];
 	for(int i = 0; i < N; ++i) {
 		bodies[i].x = bodies[i].y = bodies[i].z = i;
-		bodies[i].tag = 0;
 	}
 	return bodies;
 }
 
-void print_bodies(point3 *bodies, int N) {
+void print_bodies(float3 *bodies, int N) {
 	for(int i = 0; i < N; ++i) {
 		cout << bodies[i].x << "\t" << bodies[i].y << "\t" << bodies[i].z << endl;
 	}
@@ -79,15 +71,15 @@ void print_bodies(point3 *bodies, int N) {
 
 int main() {
 	const int N = 65000;
-	const int SIZE = N * sizeof(point3);
+	const int SIZE = N * sizeof(float3);
 
 	cout << "generating bodies..."; cout.flush();
-	point3 *bodies = mkbodies(N);
-	point3 *forces = new point3[N];
+	float3 *bodies = mkbodies(N);
+	float3 *forces = new float3[N];
 	cout << "done." << endl;
 
-	point3 *dbodies = NULL;
-	point3 *dforces = NULL;
+	float3 *dbodies = NULL;
+	float3 *dforces = NULL;
 
 	cout << "allocating device memory..."; cout.flush();
 	cudaMalloc(&dbodies, SIZE);
